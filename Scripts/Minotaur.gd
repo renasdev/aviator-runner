65eaf49140ecf284aeb381eeb010d5367f33ba0e
Renã Souza <ren.boxe@gmail.com>
2019-09-03 22:32:22 -0300

    Add Jump to Minotaur

--- Scripts/Minotaur.gd
+++ Scripts/Minotaur.gd
@@ -5,6 +5,9 @@ var direction = 1
 
 func _ready():
 	$AnimationPlayer.play("Run")
+	$FloorRayCast2D.add_exception($FrontHitArea2D)
+	$FloorRayCast2D.add_exception($HitBoxArea2D)
+	$FloorRayCast2D.add_exception($RightRayCast2D)
 
 func _process(_delta):
 	if(direction == 1):
@@ -12,14 +15,15 @@ func _process(_delta):
 		$Sprite.flip_h = false
 		$HitBoxArea2D/CollisionShape2D.position = Vector2(20, -38.5)
 		$FrontHitArea2D/CollisionShape2D.position = Vector2(10, -38.5)
+		$FloorRayCast2D.set_cast_to(Vector2(20, 50))
 	else:
 		motion.x = max(-200, motion.x - 50)
 		$Sprite.flip_h = true
 		$HitBoxArea2D/CollisionShape2D.position = Vector2(-80, -38.5)
 		$FrontHitArea2D/CollisionShape2D.position = Vector2(-90, -38.5)
+		$FloorRayCast2D.set_cast_to(Vector2(-100, 50))
 		
 	motion.y += GameConstants.GRAVITY
-	print(motion)
 	motion = move_and_slide(motion, GameConstants.UP)
 	
 	if !$AnimationPlayer.is_playing():
@@ -30,9 +34,25 @@ func _process(_delta):
 	elif $LeftRayCast2D.is_colliding() && $LeftRayCast2D.get_collider().get_name() == GameConstants.PLAYER:
 		direction = -1
 
+	if is_colliding_with_tilemap():
+		print("there is floor ")
+	elif is_on_floor():
+		print("jump!!")
+		motion.y -= 800
+	else:
+		print("shit")
+		
+func is_colliding_with_tilemap():
+	var objects_collide = []
+	if $FloorRayCast2D.is_colliding():
+		print($FloorRayCast2D.get_collider())
+		return true
+		
+	return false
 
 func _on_FrontHitArea2D_body_entered(body):
-	$AnimationPlayer.play("Simple_Attack")
+	if(body.get_name() == GameConstants.PLAYER):
+		$AnimationPlayer.play("Simple_Attack")
 
 func _on_HitBoxArea2D_body_entered(body):
 	if(body.get_name() == GameConstants.PLAYER):
