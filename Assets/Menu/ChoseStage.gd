66c01e90a3c610e56661428e42a8bc0333ca2098
Renã Souza <ren.boxe@gmail.com>
2019-09-08 17:50:29 -0300

    Update menu UI

--- Assets/Menu/ChoseStage.gd
+++ Assets/Menu/ChoseStage.gd
@@ -8,10 +8,13 @@ func _on_Button_pressed():
 	get_tree().change_scene("res://Stages/cena1.tscn")
 
 func _on_Button2_pressed():
-	get_tree().change_scene("res://Stages/cena2.tscn")	
+	get_tree().change_scene("res://Stages/cena2.tscn")
 
 func _on_Button3_pressed():
-	get_tree().change_scene("res://Stages/cena3.tscn")	
-
+	get_tree().change_scene("res://Stages/cena3.tscn")
+	
+func _on_Button4_pressed():
+	get_tree().change_scene("res://Stages/cena4.tscn")
+	
 func _on_GoBack_pressed():
 	get_tree().change_scene("res://Stages/TitleScreen.tscn")
