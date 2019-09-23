4f4b49af91e7f31f17615dad65eb9df613b600cc
Renã Souza <ren.boxe@gmail.com>
2019-09-23 20:54:27 -0300

    Add translation and enhance mobile experience

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,16 +1,37 @@
 extends Control
 
+var supported_locales := {"Português" : "pt_BR", "English" : "en"}
+
 func _ready():
 	var volumeDb = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 	print(volumeDb)
 	var volume = db2linear(volumeDb)
 	$VBoxContainer/HSplitContainer/VolumeHSlider.value = volume
+	$VBoxContainer/HSplitContainer2/FullscreenCheckBox.pressed = OS.window_fullscreen
+	
+	var menuButton := $VBoxContainer/HSplitContainer3/MenuButton
+	var popup = menuButton.get_popup()
+	popup.connect("id_pressed", self, "_on_Language_change")
+	
+	for locale in supported_locales:
+		if supported_locales[locale] == TranslationServer.get_locale():
+			menuButton.text = locale
+		popup.add_item(locale)
+		
+func _on_Language_change(id):
+	var text = $VBoxContainer/HSplitContainer3/MenuButton.get_popup().get_item_text(id)
+	TranslationServer.set_locale(supported_locales[text])
+	$VBoxContainer/HSplitContainer3/MenuButton.text = text
 
 func _on_VolumeHSlider_value_changed(value):
-	
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(value))
 	var volumeDb = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 	print(value, " ", volumeDb)
 
 func _on_GoBackButton_pressed():
 	get_tree().change_scene("res://Stages/TitleScreen.tscn")
+
+
+func _on_FullscreenCheckBox_pressed():
+	OS.window_fullscreen = $VBoxContainer/HSplitContainer2/FullscreenCheckBox.pressed
+	
--- Scripts/UIController.gd
+++ Scripts/UIController.gd
@@ -8,11 +8,20 @@ func _ready():
 func _physics_process(_delta):
 	if Input.is_action_just_pressed("ui_pause"):
 		_change_pause_state(!is_paused)
+		
+func _notification(what):
+    if what == MainLoop.NOTIFICATION_WM_FOCUS_IN:
+        _change_pause_state(true)
+    if what == MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+        _change_pause_state(true)
+
 
 func _on_ResumeButton_pressed():
 	_change_pause_state(false)
 	
 func _change_pause_state(value: bool):
+	if(is_paused == value):
+		return
 	is_paused = value
 	$ControlPause.visible = value
 	get_tree().paused = value
@@ -21,6 +30,8 @@ func _on_MainMenuButton_pressed():
 	_change_pause_state(false)
 	get_tree().change_scene("res://Stages/TitleScreen.tscn")
 
-
 func _on_PauseButton_pressed():
-	_change_pause_state(true)
+	_change_pause_state(!is_paused)
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
