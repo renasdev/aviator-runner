4fa557994e8d366f5eea2efb1329f3a918e289ef
Renã Souza <ren.boxe@gmail.com>
2019-09-23 21:00:13 -0300

    Refactor options menu

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -1,27 +1,30 @@
 extends Control
 
+onready var volume_slider := $VBoxContainer/VolumeContainer/VolumeHSlider
+onready var full_screen_checkbox := $VBoxContainer/FullScreenContainer/FullscreenCheckBox
+onready var language_menu_button := $VBoxContainer/LanguageContainer/MenuButton
+
 var supported_locales := {"Português" : "pt_BR", "English" : "en"}
 
 func _ready():
 	var volumeDb = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 	print(volumeDb)
 	var volume = db2linear(volumeDb)
-	$VBoxContainer/HSplitContainer/VolumeHSlider.value = volume
-	$VBoxContainer/HSplitContainer2/FullscreenCheckBox.pressed = OS.window_fullscreen
+	volume_slider.value = volume
+	full_screen_checkbox.pressed = OS.window_fullscreen
 	
-	var menuButton := $VBoxContainer/HSplitContainer3/MenuButton
-	var popup = menuButton.get_popup()
+	var popup = language_menu_button.get_popup()
 	popup.connect("id_pressed", self, "_on_Language_change")
 	
 	for locale in supported_locales:
 		if supported_locales[locale] == TranslationServer.get_locale():
-			menuButton.text = locale
+			language_menu_button.text = locale
 		popup.add_item(locale)
 		
 func _on_Language_change(id):
-	var text = $VBoxContainer/HSplitContainer3/MenuButton.get_popup().get_item_text(id)
+	var text = language_menu_button.get_popup().get_item_text(id)
 	TranslationServer.set_locale(supported_locales[text])
-	$VBoxContainer/HSplitContainer3/MenuButton.text = text
+	language_menu_button.text = text
 
 func _on_VolumeHSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(value))
@@ -33,5 +36,5 @@ func _on_GoBackButton_pressed():
 
 
 func _on_FullscreenCheckBox_pressed():
-	OS.window_fullscreen = $VBoxContainer/HSplitContainer2/FullscreenCheckBox.pressed
+	OS.window_fullscreen = full_screen_checkbox.pressed
 	
